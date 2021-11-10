af3db9e76e1b3199368dbd4cb5bca0ddae74700d
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-11-09 23:35:36 -0700

    added stuff

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 3005
+
+app.use(express.static('./html'))
+
+app.listen(PORT, () => {
+    console.log('App is running on', {PORT})
+})
